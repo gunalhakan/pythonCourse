66549f3816abff8012aa892e7031e2ad83e407f6
gunalhakan <gunalhakan@gmail.com>
2024-03-11 23:56:28 +0300

    ilk if elif else örneği

---
+++ if1.py
@@ -0,0 +1,12 @@
+sayi1 = int(input("ilk sayıyı girin: "))
+sayi2 = int(input("ikinci sayıyı girin: "))
+print("-"*50)
+if sayi1 > sayi2:
+    print("ilk sayı büyük")
+elif sayi2 > sayi1:
+    print("ikinci sayı büyük")
+else:
+    print("sayılar eşit")
+
+print("-"*50)
+print("Program bitti")
